1d4f7406482211df91fe2afaa7e9507f54e351a1
jiker1 <329021121@qq.com>
2019-08-29 17:08:00 +0800

    Reids

--- rmon/rmon/views/urls.py
+++ rmon/rmon/views/urls.py
@@ -4,7 +4,10 @@
 """
 from flask import Blueprint
 from rmon.views.index import IndexView
+from rmon.views.server import ServerList
+from rmon.views.server import ServerDetail
 
 api = Blueprint('api',__name__)
 api.add_url_rule('/',view_func=IndexView.as_view('index'))
-
+api.add_url_rule('/servers',view_func=ServerList.as_view('server_list'))
+app.add_url_rule('/servers/<int:object_id>',view_func=ServerDetail.as_view('server_datail'))
